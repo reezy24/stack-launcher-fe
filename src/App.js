32892ebe9c52b982d7385a08f3a5baff172b21a4
Zachary Reyes <zachary.reyes@ladbrokes.com.au>
2021-06-19 10:07:06 +1000

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  function onClick() {
+    fetch("http://localhost:8090/run")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClick}>Run</button>
     </div>
   );
 }
